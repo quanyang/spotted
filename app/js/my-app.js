2beafc50d687bf5aba4d1899042a414846502d84
jiajie <whyjayjay@gmail.com>
2016-01-24 04:34:24 +0800

    Gmaps location

--- app/js/my-app.js
+++ app/js/my-app.js
@@ -21,6 +21,7 @@ myApp.onPageInit('about', function (page) {
 });
 
 myApp.onPageInit('upload', function (page) {
+    initMap();
     getLocation();
 });
 
@@ -63,7 +64,7 @@ function getLocation() {
 
 function showPosition(position) {
     var x = document.getElementById("currLoc");
-    
+      
             var latlng = new google.maps.LatLng( position.coords.latitude, position.coords.longitude);
             var geocoder = geocoder = new google.maps.Geocoder();
             geocoder.geocode({ 'latLng': latlng }, function (results, status) {
@@ -105,6 +106,37 @@ function showhideLocation() {
     }
 }
 
-function myFunction(a, b) {
-    return a * b;
+function initMap() {
+   
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 17
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Current location');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
 }
