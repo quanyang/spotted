64b96b29f0437d3c371444bea494a2d22ff628c3
Quan Yang <quanyang@live.com>
2016-01-24 13:25:19 +0800

    test

--- app/api/spotted/controllers/ReportController.php
+++ app/api/spotted/controllers/ReportController.php
@@ -129,11 +129,7 @@ class ReportController extends Controller {
             $isLostReport = 1; //Lost Report
 
 
-            if (!preg_match("/^-?([1]?[1-7][1-9]|[1]?[1-8][0]|[1-9]?[0-9])\.{1}\d{1,6}$/",$longitude) ||!preg_match("/^-?([1-8]?[1-9]|[1-9]0)\.{1}\d{1,6}$/", $latitude) ){
-            	echo "1";
-            }
-
-            if (!InputValidator::isValidStringInput($pet_name,255,0) ||!InputValidator::isValidStringInput($image_id,255,0) || !InputValidator::isValidStringInput($latitude,255,0)|| !InputValidator::isValidStringInput($longitude,255,0)) {
+            if (!preg_match("/^-?\d{1,3}\.{1}\d*$/",$longitude) ||!preg_match("/^-?\d*\.{1}\d*$/", $latitude)||!InputValidator::isValidStringInput($pet_name,255,0) ||!InputValidator::isValidStringInput($image_id,255,0) || !InputValidator::isValidStringInput($latitude,255,0)|| !InputValidator::isValidStringInput($longitude,255,0)) {
             	echo "2";
             } 
 
