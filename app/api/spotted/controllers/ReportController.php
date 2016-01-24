c89803def689125817da373cdc40387a9fbc407d
Quan Yang <quanyang@live.com>
2016-01-24 13:42:29 +0800

    TEST

--- app/api/spotted/controllers/ReportController.php
+++ app/api/spotted/controllers/ReportController.php
@@ -129,7 +129,12 @@ class ReportController extends Controller {
             $isLostReport = 1; //Lost Report
 
 
-            if (!preg_match("/^-?\d{1,3}\.{1}\d*$/",$longitude) ||!preg_match("/^-?\d*\.{1}\d*$/", $latitude)||!InputValidator::isValidStringInput($pet_name,255,0) ||!InputValidator::isValidStringInput($image_id,255,0) || !InputValidator::isValidStringInput($latitude,255,0)|| !InputValidator::isValidStringInput($longitude,255,0)|| ($category == 3 && !InputValidator::isValidStringInput($others,255,0) ) || !InputValidator::isValidStringInput($characteristics,5000,0)) {
+            if (!preg_match("/^-?\d{1,3}\.{1}\d*$/",$longitude) ||!preg_match("/^-?\d*\.{1}\d*$/", $latitude)||!InputValidator::isValidStringInput($pet_name,255,0) ||!InputValidator::isValidStringInput($image_id,255,0) || !InputValidator::isValidStringInput($latitude,255,0)|| !InputValidator::isValidStringInput($longitude,255,0)) {
+            	echo "TEST";
+            } 
+
+            if (($category == 3 && !InputValidator::isValidStringInput($others,255,0) ) !InputValidator::isValidStringInput($characteristics,5000,0)) {
+                echo "BYE";
                 $app->render(400, ['Status' => 'Invalid input.' ]);
                 return;
             }
